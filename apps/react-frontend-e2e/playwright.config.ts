01c329a81c4309efc8ce62570f4f175dce8123d2
Andrew Mortimer <andrcmdr@protonmail.com>
2024-08-29 18:30:35 +0000

    Rename frontend to react-frontend

--- apps/react-frontend-e2e/playwright.config.ts
+++ apps/react-frontend-e2e/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npx nx run frontend:preview',
+    command: 'npx nx run react-frontend:preview',
     url: 'http://localhost:4300',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
